f78f1e939aba32fd20c8a01e08a4b8be75f12999
Andres Coronado <acoronado@endava.com>
2019-07-23 16:27:25 -0500

    prueba

--- src/com/endava/vogelMethod.java
+++ src/com/endava/vogelMethod.java
@@ -1,22 +1,30 @@
 package com.endava;
 
 public class vogelMethod {
-    int[] demanda = {30, 20, 70, 30, 60};
-    int[] oferta = {50, 60, 50, 50};
-    int[][] costos = {{16, 16, 13, 22, 17}, {14, 14, 13, 19, 15},
-            {19, 19, 20, 23, 50}, {50, 12, 50, 15, 11}};
-    int nFilas = oferta.length;
-    int nColumnas = demanda.length;
 
-    int remanente = sumaOferta(oferta);
+        int[] demanda = {30, 20, 70, 30, 60};
+        int[] oferta = {50, 60, 50, 50};
+        int[][] costos = {{16, 16, 13, 22, 17}, {14, 14, 13, 19, 15},
+                {19, 19, 20, 23, 50}, {50, 12, 50, 15, 11}};
+        int nFilas = oferta.length;
+        int nColumnas = demanda.length;
+        boolean[] filaTerminada = new boolean[nFilas];
+        boolean[] columnaTerminada = new boolean[nColumnas];
+        int[][] result = new int[nFilas][nColumnas];
 
+    //int remanente = sumaOferta(oferta);
 
-    public int sumaOferta(int[] supply){
-        int remanente=0;
-        for (int i=0; i < supply.length; i++){
-            remanente+=supply[i];
-        }
-       return remanente;
+    public vogelMethod() {
+
     }
+    public int [] largestPenalty(int longi1, int longi2, boolean esFila ){
+        int diferenciaMax= Integer.MIN_VALUE;
+        int indexDiffMax, costoMin, indexCostoMin=-1;
+
+
+    }
+
+
+
 
 }
